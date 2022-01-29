56eb04a87cf46be0f5a7ba2cfc2494c8827be3ab
David Youm <70925186+davidyoum@users.noreply.github.com>
2022-01-29 18:31:25 -0500

    f

--- main.py
+++ main.py
@@ -2,17 +2,10 @@
 #3/4/21
 #Inspiration from EngineerMan
 
-import random
+import generator
+import json
 
-while True:
-    numbers = random.randint(0, 17)
+names = json.load(open("names.json").read())
 
-    names = ["Olivia", "Emma", "Ava", "Sophia", "Isabella", "Charlotte", "Amelia", "Mia", "Willow", "Ariana", "Aaliyah", "Cora", "Clara", "Vivian", "Madeline", "Peyton", "Julia", "Rylee"]
-    name = names[numbers]
+print(names[0])
 
-    emails = ["aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com", "google.com",
-              "hotmail.com", "hotmail.co.uk", "mac.com", "me.com", "mail.com",
-              "msn.com", "live.com", "sbcglobal.net", "verizon.net", "yahoo.com"]
-    email = emails[numbers]
-
-    print(name+str(numbers)+"@"+email)
