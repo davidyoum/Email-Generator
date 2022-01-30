0b33cf0037760fada0fb6c8a3eab11c838112b0f
David Youm <70925186+davidyoum@users.noreply.github.com>
2022-01-29 19:08:43 -0500

    main

--- generator.py
+++
@@ -1,3 +0,0 @@
-email = ["aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com", "google.com",
-              "hotmail.com", "hotmail.co.uk", "mac.com", "me.com", "mail.com",
-              "msn.com", "live.com", "sbcglobal.net", "verizon.net", "yahoo.com"]
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-#Random Email Generator
+#Random Email Generator made by David Youm
 #3/4/21
-#Inspiration from EngineerMan
 
-import generator
 import json
+import random
 
-names = json.load(open("names.json").read())
+how_many_emails = 10
 
-print(names[0])
+names = json.loads(open("Email-Generator\\names.json").read())
+domains = json.loads(open("Email-Generator\domains.json").read())
+lastnames = json.loads(open("Email-Generator\lastnames.json").read())
+
+for i in range(how_many_emails):
+    random_numbers = random.randint(0, 999)
+
+    print(random.choice(names) + random.choice(lastnames) + (str)(random_numbers) + random.choice(domains))
 
